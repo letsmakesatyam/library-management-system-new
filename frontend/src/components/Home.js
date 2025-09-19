410a5a202cbf899903ca6b78cbe8d860c129d7db
Satyam Revgade <satyamrevgade2@gmail.com>
2025-09-19 08:42:24 +0530

    first commit in frontend directory

---
+++ frontend/src/components/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Welcome to Library App</h1>
+        <p>Please login or register to continue.</p>
+      </div>
+    );
+  }
+}
+
+export default Home;
