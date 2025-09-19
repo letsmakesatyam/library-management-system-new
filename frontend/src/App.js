5a95ccf2ffd2735d76c1402c6a29675d0fc40f44
letsmakesatyam <satyamrevgade2@gmail.com>
2025-09-19 11:40:48 +0000

    commited all necessary changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,39 @@
-import React, { Component } from 'react';
-import { Route, Switch } from 'react-router-dom';
-import Home from './components/Home';
+import { Switch, Route, Link } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import Login from "./components/Login";
+import Register  from "./components/Register";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
+import "./App.css";
+
+
+function App() {
+  return (
+    <div className="app-container">
+      <header className="app-header">
+        <h2>📚 Library App</h2>
+        <nav>
+          <Link to="/" className="nav-link">Home</Link>
+          <Link to="/about" className="nav-link">About</Link>
+          <Link to="/login" className="nav-link">Login</Link>
+          <Link to = "/register" className="nav-link">Register</Link>
+        </nav>
+      </header>
+
+      <main className="app-main">
         <Switch>
           <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/login" component={Login} />
+          <Route path = "/register" component = {Register} />
         </Switch>
-      </div>
-    );
-  }
+      </main>
+
+      <footer className="app-footer">
+        <p>© 2025 Library App</p>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
